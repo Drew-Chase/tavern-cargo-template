687f900bc4034ed4ab5ec297cdbaf9fa840f9d94
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-01 14:42:03 -0400

    Update Navigation component to use dynamic crate name and add maximize button

--- tauri/src/assets/components/Navigation.tsx
+++ tauri/src/assets/components/Navigation.tsx
@@ -11,12 +11,13 @@ export default function Navigation()
 
         <div className={"flex flex-row h-[2.5rem] backdrop-blur-sm sticky top-0 w-full z-[51] backdrop-saturate-150 select-none"} data-tauri-drag-region="">
             <div className={"flex flex-row"}>
-                <p className={"mx-2 mt-1 text-large font-bold select-none"} data-tauri-drag-region="">Sams Manifest Compiler</p>
+                <p className={"mx-2 mt-1 text-large font-bold select-none"} data-tauri-drag-region="">{{crate_name}}</p>
             </div>
             <div className={"flex flex-row ml-auto"}>
                 <ButtonGroup className={"h-[2rem]"}>
                     <ThemeSwitchComponent/>
                     <Button variant={"light"} className={"min-w-0 h-[2rem] text-[1rem]"} radius={"sm"} onPress={() => appWindow.minimize()}><Icon icon="material-symbols:minimize-rounded"/></Button>
+                    <Button variant={"light"} className={"min-w-0 h-[2rem] text-[.7rem]"} radius={"sm"} onPress={() => appWindow.toggleMaximize()}><Icon icon="material-symbols:square-outline-rounded"/></Button>
                     <Button variant={"light"} color={"danger"} className={"min-w-0 h-[2rem] text-[1rem]"} radius={"sm"} onPress={() => appWindow.close()}><Icon icon="material-symbols:close-rounded"/></Button>
                 </ButtonGroup>
             </div>
