a42799b384ec3921b984c28515a2a2f748f59e09
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-01 13:51:53 -0400

    Add custom titlebar option and enhance Navigation component with toast notifications

--- tauri/src/assets/components/Navigation.tsx
+++ tauri/src/assets/components/Navigation.tsx
@@ -1,3 +1,29 @@
+{% if custom_chrome == true %}
+import {Button, ButtonGroup} from "@heroui/react";
+import {ThemeSwitchComponent} from "../providers/ThemeProvider.tsx";
+import {getCurrentWindow} from "@tauri-apps/api/window";
+import {Icon} from "@iconify-icon/react";
+
+export default function Navigation()
+{
+    const appWindow = getCurrentWindow();
+    return (
+
+        <div className={"flex flex-row h-[2.5rem] backdrop-blur-sm sticky top-0 w-full z-[51] backdrop-saturate-150 select-none"} data-tauri-drag-region="">
+            <div className={"flex flex-row"}>
+                <p className={"mx-2 mt-1 text-large font-bold select-none"} data-tauri-drag-region="">Sams Manifest Compiler</p>
+            </div>
+            <div className={"flex flex-row ml-auto"}>
+                <ButtonGroup className={"h-[2rem]"}>
+                    <ThemeSwitchComponent/>
+                    <Button variant={"light"} className={"min-w-0 h-[2rem] text-[1rem]"} radius={"sm"} onPress={() => appWindow.minimize()}><Icon icon="material-symbols:minimize-rounded"/></Button>
+                    <Button variant={"light"} color={"danger"} className={"min-w-0 h-[2rem] text-[1rem]"} radius={"sm"} onPress={() => appWindow.close()}><Icon icon="material-symbols:close-rounded"/></Button>
+                </ButtonGroup>
+            </div>
+        </div>
+    );
+}
+{% else %}
 import React from "react";
 import {Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle} from "@heroui/react";
 import {ThemeSwitchComponent} from "../providers/ThemeProvider.tsx";
@@ -47,3 +73,4 @@ export default function Navigation()
             </NavbarMenu>
         </Navbar>);
 }
+{% endif %}
