386da69c6bff6a35954ea494f11e8ea71faf7cca
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-07-01 14:13:27 -0400

    Add ToastProvider with timeout progress and refactor Route rendering based on custom chrome setting

--- tauri/src/main.tsx
+++ tauri/src/main.tsx
@@ -5,10 +5,9 @@ import $ from "jquery";
 
 import "./assets/css/index.css";
 import Home from "./assets/pages/Home.tsx";
-import About from "./assets/pages/About.tsx";
 import Navigation from "./assets/components/Navigation.tsx";
 import {ThemeProvider} from "./assets/providers/ThemeProvider.tsx";
-import {HeroUIProvider} from "@heroui/react";
+import {HeroUIProvider, ToastProvider} from "@heroui/react";
 
 
 ReactDOM.createRoot($("#root")[0]!).render(
@@ -24,15 +23,39 @@ ReactDOM.createRoot($("#root")[0]!).render(
 export function MainContentRenderer()
 {
     const navigate = useNavigate();
+    $(window).on("contextmenu", e => e.preventDefault());
     return (
         <HeroUIProvider navigate={navigate}>
-            <Navigation/>
-            <Routes>
-                <Route>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/about" element={<About/>}/>
-                </Route>
-            </Routes>
+            {% raw %}
+            <ToastProvider
+                placement={"bottom-right"}
+                toastProps={{
+                    shouldShowTimeoutProgress: true,
+                    timeout: 3000,
+                    variant: "flat"
+                }}
+            />
+            {% endraw %}
+            <main className={"flex flex-col p-0 m-0"}>
+                <Navigation/>
+                {% if custom_chrome == true %}
+                <div className={"flex flex-row w-full max-h-[calc(100vh-2.5rem)] h-screen overflow-y-hidden p-0 m-0"} data-tauri-drag-region="">
+                    <Routes>
+                        <Route>
+                            <Route path="/" element={<Home/>}/>
+                        </Route>
+                    </Routes>
+                </div>
+                {% else %}
+                <div className={"flex flex-row w-full h-screen p-0 m-0"}>
+                    <Routes>
+                        <Route>
+                            <Route path="/" element={<Home/>}/>
+                        </Route>
+                    </Routes>
+                </div>
+                {% endif %}
+            </main>
         </HeroUIProvider>
     );
 }
